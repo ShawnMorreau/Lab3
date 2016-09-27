85c0822e8801126dd9150fdbd103dc32a87bf2b0
shawnmorreau <shawnmorreau@cmail.carleton.ca>
2016-09-27 12:03:59 -0400

    added a new buddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,25 +1,26 @@
 import java.util.*;
 public class AddressBook {
 	static BuddyInfo newBuddy;
-	static ArrayList<BuddyInfo> buddyBook;
+	static ArrayList<BuddyInfo> addressBook;
 	public AddressBook(){
 		
 	}
 	public void addBuddy(BuddyInfo buddy){
 		if(buddy != null){
-			buddyBook.add(buddy);
+			addressBook.add(buddy);
 		}
 	}
 	public void removeBuddy(int index){
-		if(index >= 0 && index < buddyBook.size()){
-			buddyBook.remove(index);
+		if(index >= 0 && index < addressBook.size()){
+			addressBook.remove(index);
 		}
 	}
 	public static void main(String args[]){
 		System.out.println("Address Book");
-		newBuddy = new BuddyInfo();
-		buddyBook = new ArrayList<BuddyInfo>();
-		buddyBook.add(newBuddy);
-		buddyBook.remove(0);
-	}
+		newBuddy = new BuddyInfo("john","Carleton",123);
+		addressBook = new ArrayList<BuddyInfo>();
+		addressBook.add(newBuddy);
+		addressBook.remove(0);
+		addressBook.add(newBuddy);
+	}//https://github.com/ShawnMorreau/Lab3.git
 }
